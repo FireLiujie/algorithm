385bf9e5189de4b4d1a551e7590432cf04229717
FireLiujie <445198516@qq.com>
2019-06-29 00:46:36 +0800

    [NEW] 新增检测回文数函数

--- study/1.js
+++ study/1.js
@@ -4,4 +4,26 @@
  **/
 
 // 方法一
-function Palindrome(x) {}
+var num = 123454321
+
+function palindrome(x) {
+  if (x < 0 || (x % 10 == 0 && x != 0)) {
+    return false
+  }
+  var div = 1
+  while (x / div > 10) {
+    div *= 10
+  }
+  while (x > 0) {
+    var left = ~~(x / div)
+    var right = ~~(x % 10)
+    if (left != right) {
+      return false
+    }
+    x = ~~((x % div) / 10)
+    div = ~~(div / 100)
+  }
+  return true
+}
+
+console.log(palindrome(num))
